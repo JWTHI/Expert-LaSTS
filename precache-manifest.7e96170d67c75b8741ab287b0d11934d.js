eec4f98d9e21cf284e70347380ef2ba0a8a76fb6
JWTHI <jonas.wurst@thi.de>
2022-01-04 10:31:14 +0100

    Init

---
+++ precache-manifest.7e96170d67c75b8741ab287b0d11934d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c13869f53808dfc54b1",
+    "url": "/Expert-LaSTS/static/css/main.5a8e43af.chunk.css"
+  },
+  {
+    "revision": "0c13869f53808dfc54b1",
+    "url": "/Expert-LaSTS/static/js/main.0c13869f.chunk.js"
+  },
+  {
+    "revision": "d41fcd46c642c4af341e",
+    "url": "/Expert-LaSTS/static/js/1.d41fcd46.chunk.js"
+  },
+  {
+    "revision": "46d7ddf8d1dffd544978",
+    "url": "/Expert-LaSTS/static/js/runtime~main.46d7ddf8.js"
+  },
+  {
+    "revision": "f858b8a00881fc4943175df8eeafe8b9",
+    "url": "/Expert-LaSTS/index.html"
+  }
+];
